396f1885dac92ace10ec6124e4754b079f1ec123
adikhaled <khldzmnd@gmail.com>
2022-12-01 23:40:32 -0500

    update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,16 +5,15 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
-	@RunWith(Cucumber.class)
-	@CucumberOptions(
-			tags = "@run",
-			features = "src/test/java/features",
-			glue = {"base","steps"},
-			monochrome = true,
-			dryRun = false,
-			plugin = {"html:target/index.html", "json:target/report.json"})
-	public class TestRunner {
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		tags = "@run",
+		features = "src/test/java/features",
+		glue = {"base","steps"},
+		monochrome = true,
+		dryRun = false,
+		plugin = {"html:target/index.html", "json:target/report.json"})
+public class TestRunner {
 	
 	
 
